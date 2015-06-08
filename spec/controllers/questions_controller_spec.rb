70b4325a3e1e00318f29f9cfee53c170da372da6
adutka <adutka@ukr.net>
2015-06-08 13:22:54 +0300

    add create with invalid attr question

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -73,7 +73,16 @@ expect(assigns(:question)).to eq question
         expect(response).to redirect_to question_path(assigns(:question))
       end
     end
+
+    context 'with invalid attributes' do
+      it 'doesn"t save the question' do
+        expect { post :create, question: attributes_for(:invalid_question) }.to_not change(Question, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, question: attributes_for(:invalid_question)
+        expect(response).to render_template :new
+      end
+    end
   end
-
-
 end
