611916f1d9b5db4002ca98a83bd07ada5ccab066
adutka <adutka@ukr.net>
2015-07-08 11:29:11 +0300

    aded webkit

--- spec/features/acceptance_helper.rb
+++ spec/features/acceptance_helper.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 require 'spec_helper'
 RSpec.configure do |config|
+  Capybara.javascript_driver = :webkit
 
   config.include FeaturesHelpers, type: :feature
   config.use_transactional_fixtures = false #dates not save in db wit gem 'database_cleaner'
