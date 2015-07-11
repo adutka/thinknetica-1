211c07990c02e873e3a25ee82375bdb8d0aa6bad
adutka <adutka@ukr.net>
2015-07-11 11:51:46 +0300

    added methods best and cancel_best in answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,7 @@
 
 class AnswersController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :load_question_and_answer, only: [:index, :new, :create, :update, :best, :cancel_best]
 
   def index
     @question = Question.find(params[:question_id])
@@ -31,9 +32,35 @@ class AnswersController < ApplicationController
     end
   end
 
+  def best
+      if @question.user == current_user
+        @answer.select_best
+        @answers = @question.answers
+        render 'answers'
+      else
+        redirect_to root_url
+      end
+    end
+
+    def cancel_best
+      if @question.user == current_user
+        @answer.cancel_best
+        @answers = @question.answers
+        render 'answers'
+      else
+        redirect_to root_url
+      end
+    end
+
+
   private
 
   def answer_params
     params.require(:answer).permit(:body)
+  end
+
+  def load_question_and_answer
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id]) if params[:id]
   end
 end
