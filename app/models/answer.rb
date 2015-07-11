114d691a5bed3df28b014ad12a4ad9b95aa0cfe9
adutka <adutka@ukr.net>
2015-07-11 13:01:16 +0300

    added true for best answer in model answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,4 +5,13 @@ class Answer < ActiveRecord::Base
   validates :question_id, presence: true
   validates :body, length: { in: 5..1000 }, presence: true
   validates :user_id, presence: true
+
+  def select_best
+    question.answers.update_all(best: false)
+    update!(best: true)
+  end
+
+  def cancel_best
+    update!(best: false)
+  end
 end
