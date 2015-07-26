8489e192bf9364189494763fef2ae7f83c9f6437
adutka <adutka@ukr.net>
2015-07-26 19:03:27 +0300

    added files to answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,6 +12,7 @@ class AnswersController < ApplicationController
     # @question = Question.find(params[:question_id])
     @answer = @question.answers.new(answer_params)
     @answer.user = current_user
+    # byebug
 
     @message = if @answer.save
       'Your answer successfully created.'
