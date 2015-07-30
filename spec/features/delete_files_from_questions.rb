890b3ed6bc6bd7704ac1531b91c48bcb190c18e5
adutka <adutka@ukr.net>
2015-07-31 02:22:25 +0300

    deleted files from Q

--- spec/features/delete_files_from_questions.rb
+++ spec/features/delete_files_from_questions.rb
@@ -1,6 +1,6 @@
 require_relative 'acceptance_helper'
 
-feature 'Add files to question', %q{
+feature 'Deltw files from question', %q{
   In order to illustrate myquestion
   As an question's athor
   I'd like to be able to attach files
@@ -11,32 +11,6 @@ feature 'Add files to question', %q{
   background do
     sign_in(user)
     visit questions_path
-  end
-
-  scenario 'user adds file when ask question' do
-    click_on 'Ask question'
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'Text text lala'
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-
-    click_on 'Create'
-    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
-  end
-
-  scenario 'user adds files when ask question', js: true do
-    click_on 'Ask question'
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'Text text lala'
-
-    click_on 'Add an attachment'
-
-    inputs = all('input[type="file"]')
-    inputs[0].set("#{Rails.root}/spec/spec_helper.rb")
-    inputs[1].set("#{Rails.root}/spec/rails_helper.rb")
-
-    click_on 'Create'
-    expect(page).to have_link 'spec_helper.rb'
-    expect(page).to have_link 'rails_helper.rb'
   end
 
   scenario 'user removes files in question', js: true do
@@ -67,4 +41,3 @@ feature 'Add files to question', %q{
     expect(page).to_not have_link '1.txt'
 
   end
-end
